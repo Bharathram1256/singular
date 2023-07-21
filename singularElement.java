fa2c2c19885676cd72215b06c10a24dd029b8a47
Bharathram1256 <maruboyinibharathkumar@gmail.com>
2023-07-21 11:02:26 +0530

    in java

--- singularElement.java
+++ singularElement.java
@@ -1,5 +1,30 @@
-public class singularElement{
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class singularElement {
+
+
+    public static void arrayIn(int size,Scanner sc) {
+        int arr[] = new int[size];
+        // Scanner a = new Scanner(System.in);
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
         
+
+    }
+
+    
+     public static void main(String[] args) {
+        // user input array
+        Scanner sc = new Scanner(System.in);
+
+        // taking the size of an array as input.
+        int size = sc.nextInt();
+
+        // print the array
+         arrayIn(size,sc);
     }
 }
